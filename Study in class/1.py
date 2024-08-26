88c4669c5475b9a9933fc650b8296951abf07c39
Sang Virgo <tansang06092004@gmail.com>
2024-08-26 23:34:04 +0700

    26-8-2024

--- Study in class/1.py
+++ Study in class/1.py
@@ -1,4 +1,4 @@
-import math
+# import math
 
 # print(math.trunc(-3.14))
 
@@ -17,6 +17,11 @@ import math
 
 
 # list
+# Definition:
+# - ordered collections of arbitrarily typed objects
+# - no fixed size
+# - mutable: changed in place, grow, and shrink
+
 # l=[123, 'spam', 1.23]
 # print(len(l))
 # print(l[:-1])
@@ -56,7 +61,7 @@ import math
 # L1[0]=4
 # print(L2)
 
-import copy
+# import copy
 # y=[1,2,3]
 # x1=copy.copy(y)
 # x2=copy.deepcopy(y)
@@ -66,9 +71,9 @@ import copy
 # y=[[4, [11, 56],5], [6,7]]
 # x1=copy.copy(y)
 # x2=copy.deepcopy(y)
+# y[0]=1  
 # x3=y[:]
 # # y[3][1][0]=100
-# y[0]=1  
 # print(x1)
 # print(x2) # => different
 # print(x3) # => same     
@@ -118,12 +123,114 @@ import copy
 # }
 
 
-T = ('spam', 3.0, [11, 22, 33])
-print(T[1])
+# T = ('spam', 3.0, [11, 22, 33])
+# print(T[1])
 
-print(T[2])
+# print(T[2])
 
-print(T[2][1])
+# print(T[2][1])
 
-T[2][1] = 15
-print(T)
+# T[2][1] = 15
+# print(T)
+
+
+# thisdict={  "brand": "Ford",
+#             "model": "Mustang",
+#             "year": 1964,
+#             "temp": [1,[2, 4],3]
+# }
+
+# del thisdict['brand']
+# print(thisdict)
+
+
+# 
+
+
+# Tuple trong Python là một kiểu dữ liệu dùng để lưu trữ một nhóm các phần tử. 
+# Các phần tử trong một tuple có thể thuộc các kiểu dữ liệu khác nhau (int, 
+# float, string, v.v.). Tuple rất giống với danh sách (list), nhưng có một điểm 
+# khác biệt quan trọng là tuple không thể thay đổi (immutable) sau khi đã được 
+# tạo ra, tức là bạn không thể thay đổi giá trị của các phần tử trong tuple.
+
+# T=(1,2,3)
+# print(T)
+
+# print(type(T))
+
+# set
+# set(): not duplicate, not sorted,
+# mutable: add, remove, update
+
+X=set('spamm')
+Y={'h','a','m'}
+# print(X,Y)
+
+# print(X&Y)
+
+# print(X|Y)
+
+# print(X-Y)
+
+# print(X>Y)
+# thisset = {"apple", "banana", "cherry"}
+# tropical = {"pineapple", "mango", "papaya"}
+
+# # thisset.update(tropical)
+# # print(thisset)
+
+# thisset.discard("banana")
+# print(thisset)
+
+
+
+# file
+# open file
+# ○ "r" - read, opens a file for reading, error
+# if the file does not exist
+# ○ "a" - append - opens a file for
+# appending, creates the file if it does not
+# exist
+# ○ "w" - write - opens a file for writing,
+# creates the file if it does not exist
+# ○ "x" - create - creates the specified file,
+# returns an error if the file exists
+
+# ● read/write file
+# ● close file
+
+
+# f=open('data.txt', 'w', encoding='utf-8')
+# f.write('Hello\n')
+
+# f.write("Sơn ngu vãi lon")
+
+# f.close()
+
+
+# f=open('data.txt', 'r', encoding='utf-8')
+
+# print(f.read())
+
+
+# tmp_dict={
+#     0: "ford",
+#     1: False,
+#     3: 19654,
+#     2: ["red", "white", "blue"],
+#     4: (1, 2, 3, 4)
+# }
+
+# tmp_dict[0]+=" Edition 1"
+# tmp_dict[2]=tmp_dict[2][1::-1]+["yellow"]
+# tmp_dict[4]=tmp_dict[4]+(12)
+# print(tmp_dict)
+
+
+
+# a, b, c, d, e, f = 'sonngu'
+# print(a, b, c, d, e, f)
+
+fruits = ("apple", "banana", "cherry", "orange")
+(*variable_1, variable_2, variable_3) = fruits
+print(variable_1[-1::-1], variable_2, variable_3)
