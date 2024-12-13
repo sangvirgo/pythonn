8c0025547550009ef1d8b6f1337f69921bdb23ad
tansang06092004@gmail.com <tansang06092004@gmail.com>
2024-12-13 23:54:37 +0700

    13/12/2024

---
+++ reviewFinal/21.py
@@ -0,0 +1,18 @@
+import functools
+
+n=int(input())
+
+listS=list(map(float, input().split()))
+
+maxS=max(listS)
+minS=min(listS)
+
+listF=list(filter(lambda p:p!=maxS and p!=minS, listS))
+
+sumS=functools.reduce(lambda a, b: a+b, listF)
+
+print(round(sumS/(len(listF)), 2))
+'''
+6
+6.75 8 9.2 7.25 7.75 6.75
+'''
