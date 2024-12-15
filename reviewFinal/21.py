c69d28d75473ac71e39c8370bc2e26fedbdb83fc
tansang06092004@gmail.com <tansang06092004@gmail.com>
2024-12-15 23:58:46 +0700

    15/12/2024

--- reviewFinal/21.py
+++ reviewFinal/21.py
@@ -12,6 +12,10 @@ listF=list(filter(lambda p:p!=maxS and p!=minS, listS))
 sumS=functools.reduce(lambda a, b: a+b, listF)
 
 print(round(sumS/(len(listF)), 2))
+
+
+
+
 '''
 6
 6.75 8 9.2 7.25 7.75 6.75
