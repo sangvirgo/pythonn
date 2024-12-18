eaac8acc9bcb3b03d11f4e535e06a55621a45a3b
tansang06092004@gmail.com <tansang06092004@gmail.com>
2024-12-18 23:52:54 +0700

    18/12/2024

--- reviewFinal/22.py
+++ reviewFinal/22.py
@@ -1,5 +1,23 @@
-  dgsdkfasfasdfasdfjas
-  asfasdlfa
-  sfasfdkas
-  fdas
-  fasdfas
+def sum_of_num(x):
+  s=0
+  while x>0:
+    s+=x%10
+    x//=10
+  return s
+
+def solve():
+  t=int(input())
+  n=int(input())
+  arr=list(map(int, input().split()))
+  arr.sort(key= lambda x: (sum_of_num(x), x))
+  # print(*(x for x in arr), end=" ")
+  for x in arr:
+    print(x, end=" ")
+
+solve()
+'''
+1
+8
+143 43 22 99 7 9 1111 10000000
+
+'''
